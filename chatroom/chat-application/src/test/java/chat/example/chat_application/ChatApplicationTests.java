a80c31270ebf40375f5ee055e087a310257d0e1d
jeromebaldo <baldo.jerome@gmail.com>
2024-10-07 21:58:02 -0400

    first commit

---
+++ chatroom/chat-application/src/test/java/chat/example/chat_application/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.example.chat_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
