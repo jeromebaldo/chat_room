bddc019205550f84095c6da41e895db8d2b7d5e4
jeromebaldo <baldo.jerome@gmail.com>
2024-10-13 16:25:41 -0400

    separate html and add a new path

--- chatroom/chat-application/src/main/java/chat/example/chat_application/ChatApplication.java
+++ chatroom/chat-application/src/main/java/chat/example/chat_application/ChatApplication.java
@@ -2,14 +2,10 @@ package chat.example.chat_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-
 
 @SpringBootApplication
 public class ChatApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
--- chatroom/chat-application/src/main/java/chat/example/chat_application/WebController.java
+++ chatroom/chat-application/src/main/java/chat/example/chat_application/WebController.java
@@ -1,54 +1,18 @@
 package chat.example.chat_application;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 class WebController {
 
     @GetMapping("/")
     public String index() {
-        return "<html>" +
-                "<head>" +
-                "<title>Chat Application</title>" +
-                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.1/sockjs.min.js\"></script>" +
-                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.min.js\"></script>" +
-                "<script type=\"text/javascript\">" +
-                "var stompClient = null;" +
-
-                "function connect() {" +
-                "    var socket = new SockJS('/chat');" +
-                "    stompClient = Stomp.over(socket);" +
-                "    stompClient.connect({}, function (frame) {" +
-                "        console.log('Connected: ' + frame);" +
-                "        stompClient.subscribe('/broadcast/messages', function (message) {" +
-                "            showMessage(message.body);" +
-                "        });" +
-                "    });" +
-                "}" +
-
-                "function sendMessage() {" +
-                "    var message = document.getElementById('message').value;" +
-                "    stompClient.send(\"/client/sendMessage\", {}, message);" +
-                "}" +
-
-                "function showMessage(message) {" +
-                "    var response = document.getElementById('response');" +
-                "    var p = document.createElement('p');" +
-                "    p.appendChild(document.createTextNode(message));" +
-                "    response.appendChild(p);" +
-                "}" +
-                "</script>" +
-                "</head>" +
-                "<body>" +
-                "<h1>Simple Chat Application</h1>" +
-                "<input type=\"text\" id=\"message\" placeholder=\"Enter your message here...\">" +
-                "<button onclick=\"sendMessage()\">Send</button>" +
-                "<div id=\"response\"></div>" +
-                "<script type=\"text/javascript\">" +
-                "connect();" +
-                "</script>" +
-                "</body>" +
-                "</html>";
+        return "connexion";
     }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }    
 }
